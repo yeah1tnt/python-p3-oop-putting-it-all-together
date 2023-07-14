d4f8f92fd8608f599c6e851f96aa1a2d8e3aaf06
Tong Tang <pichip22@hotmail.com>
2023-07-14 06:15:49 -0400

    completed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title="zero",page_count="0"):
+        self.title = title
+        self.page_count = page_count
+
+    def get_title(self):
+        return self._title
     
-        
+    def set_title(self,title):
+        if(isinstance(title, str)):
+            self._title = title
+        else:
+            print("title must be a string")
+    
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self,page_count):
+        if(isinstance(page_count, int)):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_page_count,set_page_count)
+    title = property(get_title,set_title)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    def get_brand(self):
+        return self._brand
+    
+    def get_size(self):
+        return self._size
+    
+    def set_brand(self, brand):
+        self._brand = brand
+
+    def set_size(self, size):
+        if(isinstance(size, int)):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        Shoe.condition = "New"
+
+    brand = property(get_brand, set_brand)
+    size = property(get_size, set_size)
